0dc1eb1e377ddea26e33625f22e0623711c32ad9
Michael H <michael@michaelhall.tech>
2023-06-06 07:05:54 -0400

    clean this up slightly

--- dicemath.zig
+++ dicemath.zig
@@ -14,16 +14,16 @@ export fn ev_xdy_keep_best_n(x: u16, y: u16, n: u16) f64 {
 
     for (0..(n)) |k| {
         for (1..(y + 1)) |j| {
-            var _j = @intToFloat(f64, j);
+            const _j = @intToFloat(f64, j);
 
             var inner_sum: f64 = 0;
 
             for (0..(k + 1)) |i| {
-                var _i = @intToFloat(f64, i);
-                var bc: f64 = @intToFloat(f64, binomial_coeff(x, i));
+                const _i = @intToFloat(f64, i);
+                const bc: f64 = @intToFloat(f64, binomial_coeff(x, i));
 
-                var p1: f64 = std.math.pow(f64, ((_y - _j) / _y), _i) * std.math.pow(f64, (_j / _y), (_x - _i));
-                var p2: f64 = std.math.pow(f64, ((_y - _j + 1) / _y), _i) * std.math.pow(f64, ((_j - 1) / _y), _x - _i);
+                const p1: f64 = std.math.pow(f64, ((_y - _j) / _y), _i) * std.math.pow(f64, (_j / _y), (_x - _i));
+                const p2: f64 = std.math.pow(f64, ((_y - _j + 1) / _y), _i) * std.math.pow(f64, ((_j - 1) / _y), _x - _i);
 
                 inner_sum = inner_sum + bc * (p1 - p2);
             }
@@ -42,16 +42,16 @@ export fn ev_xdy_keep_worst_n(x: u16, y: u16, n: u16) f64 {
 
     for (1..(n + 1)) |k| {
         for (1..(y + 1)) |j| {
-            var _j = @intToFloat(f64, j);
+            const _j = @intToFloat(f64, j);
 
             var inner_sum: f64 = 0;
 
             for (0..(x - k + 1)) |i| {
-                var _i = @intToFloat(f64, i);
-                var bc: f64 = @intToFloat(f64, binomial_coeff(x, i));
+                const _i = @intToFloat(f64, i);
+                const bc: f64 = @intToFloat(f64, binomial_coeff(x, i));
 
-                var p1: f64 = std.math.pow(f64, ((_y - _j) / _y), _i) * std.math.pow(f64, (_j / _y), (_x - _i));
-                var p2: f64 = std.math.pow(f64, ((_y - _j + 1) / _y), _i) * std.math.pow(f64, ((_j - 1) / _y), _x - _i);
+                const p1: f64 = std.math.pow(f64, ((_y - _j) / _y), _i) * std.math.pow(f64, (_j / _y), (_x - _i));
+                const p2: f64 = std.math.pow(f64, ((_y - _j + 1) / _y), _i) * std.math.pow(f64, ((_j - 1) / _y), _x - _i);
 
                 inner_sum = inner_sum + bc * (p1 - p2);
             }
